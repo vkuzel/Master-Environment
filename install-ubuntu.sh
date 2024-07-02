6da0b48698a9732db0f650d4b87395257cede141
vkuzel <vkuzel@gmail.com>
2024-07-02 11:13:45 +0200

    Replace Lite-XL w/ GEdit.

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -156,76 +156,6 @@ configure_mako() {
 	fi
 }
 
-install_lite_xl() {
-	local editorUrl="$1"
-	local installDir=/opt/lite-xl
-	local archiveFile=$(echo "$editorUrl" | grep -Eo "[^/]+$")
-
-	info "=== Install Lite XL $archiveFile ==="
-
-	if [[ ! -d "$installDir" ]]; then
-		pushd /tmp > /dev/null
-		curl --location "$editorUrl" --output "$archiveFile" || fail "Cannot download $editorUrl"
-		tar -xzf "$archiveFile" || fail "Cannot unpack $archiveFile"
-
-		sudo mv lite-xl "$installDir" || fail "Cannot install into $installDir"
-		sudo chown -R root:root "$installDir"
-
-		rm "$archiveFile"
-		popd > /dev/null
-	else
-		info "Already installed"
-	fi
-}
-
-install_lite_xl_plugin() {
-	local pluginUrl="$1"
-	local pluginsDir="$HOME/.config/lite-xl/plugins"
-	local pluginFile=$(echo "$pluginUrl" | grep -Eo "[^/]+$" | grep -Eo "^[^?]+")
-
-	info "=== Install Lite XL plugin: $pluginFile ==="
-
-	if [[ ! -f "$pluginsDir/$pluginFile" ]]; then
-		pushd /tmp > /dev/null
-		mkdir -p "$pluginsDir" || fail "Cannot create dir $pluginsDir"
-		curl --location "$pluginUrl" --output "$pluginFile" || fail "Cannot download $pluginUrl"
-		mv "$pluginFile" "$pluginsDir" || fail "Cannot install into $pluginsDir"
-		popd > /dev/null
-	else
-		info "Already installed"
-	fi
-}
-
-install_lite_xl_desktop_file() {
-	local desktopFile="lite-xl.desktop"
-
-	info "=== Install Lite XL desktop file: $desktopFile ==="
-
-	pushd /tmp > /dev/null
-	cat <<- 'DESKTOP_FILE' > "$desktopFile"
-	[Desktop Entry]
-	Type=Application
-	Name=Lite XL
-	GenericName=Text Editor
-	Comment=Lite XL Text Editor
-
-	Categories=Utility;Development;TextEditor;IDE
-	Keywords=Text;Editor;
-	MimeType=text/plain;text/x-chdr;text/x-csrc;text/x-c++hdr;text/x-c++src;text/x-java;text/x-dsrc;text/x-pascal;text/x-perl;text/x-python;application/x-php;application/x-httpd-php3;application/x-httpd-php4;application/x-httpd-php5;application/xml;text/html;text/css;text/x-sql;text/x-diff;
-
-	Exec=/opt/lite-xl/lite-xl
-	Terminal=false
-DESKTOP_FILE
-	if command -v desktop-file-install &> /dev/null; then
-		# Gnome desktop
-		sudo desktop-file-install --rebuild-mime-info-cache "$desktopFile"
-	elif [[ ! -f "/usr/share/applications/$desktopFile" ]]; then
-		sudo cp -f "$desktopFile" /usr/share/applications
-	fi
-	rm "$desktopFile"
-	popd > /dev/null
-}
-
 create_directory_structure() {
 	info "=== Create directory structure ==="
 	local src_dir=$1
@@ -327,10 +257,7 @@ create_links $SRC_DIR $DST_DIR
 configure_mako $SRC_DIR
 
 # Office utils
-install_lite_xl "https://github.com/lite-xl/lite-xl/releases/download/v2.1.3/lite-xl-v2.1.3-addons-linux-x86_64-portable.tar.gz"
-install_lite_xl_plugin "https://raw.githubusercontent.com/lite-xl/lite-xl-plugins/master/plugins/autosave.lua"
-install_lite_xl_plugin "https://github.com/lite-xl/lite-xl-plugins/blob/master/plugins/gitstatus.lua?raw=1"
-install_lite_xl_desktop_file
+install_apt_package gedit
 
 # Utils
 install_apt_package whois
