5d68029cc9c7e97fad5fef8e37ed1408f7c7739e
vkuzel <vkuzel@gmail.com>
2025-03-07 11:08:08 +0100

    Bind Alt+Print key to (Grim) focused-window screenshot.

--- home/.config/sway/screenshot.sh
+++ home/.config/sway/screenshot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-region="$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | "\(.rect.x),\(.rect.y) \(.rect.width)x\(.rect.height)"')"
+if [[ "$1" == "--focused-window" ]]; then
+	region="$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | "\(.rect.x),\(.rect.y) \(.rect.width)x\(.rect.height)"')"
+else
+	# whole screen
+	region="$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | "\(.rect.x),\(.rect.y) \(.rect.width)x\(.rect.height)"')"
+fi
 
 GRIM_DEFAULT_DIR="$HOME/Downloads" grim -g "$region"
+
