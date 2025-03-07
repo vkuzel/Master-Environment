b19f2704a101f1e39194bb1ea7fb1d8aa704c442
vkuzel <vkuzel@gmail.com>
2025-03-07 11:23:53 +0100

    Name screenshot files.

--- home/.config/sway/screenshot.sh
+++ home/.config/sway/screenshot.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [[ "$1" == "--focused-window" ]]; then
+	windowName=$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .name')
+	fileName="screenshot-$windowName-$(date +'%Y-%m-%d_%H-%M-%S').png"
 	region="$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | "\(.rect.x),\(.rect.y) \(.rect.width)x\(.rect.height)"')"
 else
 	# whole screen
+	workspaceNo=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .current_workspace')
+	fileName="screenshot-workspace-$workspaceNo-$(date +'%Y-%m-%d_%H-%M-%S').png"
 	region="$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | "\(.rect.x),\(.rect.y) \(.rect.width)x\(.rect.height)"')"
 fi
 
-GRIM_DEFAULT_DIR="$HOME/Downloads" grim -g "$region"
+grim -g "$region" "$HOME/Downloads/$fileName"
 
