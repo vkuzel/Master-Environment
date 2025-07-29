b0f6fd62ef27d31a9a5794f208e18697f9a9df9f
vkuzel <vkuzel@gmail.com>
2025-07-29 10:42:45 +0200

    Don't animate images.

--- home/.local/bin/view-images.sh
+++ home/.local/bin/view-images.sh
@@ -3,7 +3,7 @@
 find -iname "*.jpeg" -o -iname "*.jpg" -o -iname "*.gif" -o -iname "*.bmp"
 
 while IFS= read -r -d '' file; do
-	chafa --speed 1000fps "$file"
+	chafa --animate off "$file"
 	read -p "Delete file $file [y/N] " answer < /dev/tty
 	if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
 		rm "$file"
