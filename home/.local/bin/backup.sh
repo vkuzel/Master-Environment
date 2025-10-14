2867058a101d5ea868e3f6028ac543e9771fad7f
vkuzel <vkuzel@gmail.com>
2025-10-14 16:43:29 +0200

    Backup script - documents.

---
+++ home/.local/bin/backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Backup user data to backup device
+
+error() {
+  echo "$1"
+  exit 1
+}
+
+backupDevice="/media/veracrypt1"
+
+[[ -d "$backupDevice" ]] || error "Backup device $backupDevice does not exist!"
+
+hostname=$(hostname)
+currentMonth=$(date "+%Y-%m")
+backupDir="$backupDevice/$hostname/$currentMonth"
+
+echo "Backup into $backupDir"
+rsync \
+	--archive \
+	--delete \
+	--ignore-missing-args \
+	--mkpath \
+	--exclude="node_modules/" \
+	--exclude=".gradle" \
+	--exclude=".kotlin" \
+	--exclude="build" \
+	"$HOME/Documents" \
+	"$HOME/projects" \
+	"$HOME/Music" \
+	"$HOME/Videos" \
+	"$backupDir"
