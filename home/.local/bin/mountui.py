a4936e63fcefa11fee695caa0bd2aa75a75143f0
vkuzel <vkuzel@gmail.com>
2025-11-19 18:01:19 +0100

    Mountable device resolution stub.

--- home/.local/bin/mountui.py
+++ home/.local/bin/mountui.py
@@ -11,16 +11,38 @@ class BlockDevice():
     path: Optional[str]
     fstype: Optional[str]
     label: Optional[str]
+    tran: Optional[str]
     mountpoint: Optional[str]
     type: Optional[str]
     size: Optional[int]
     children: Optional[List["BlockDevice"]]  # recursive definition
 
 
+@dataclass
+class MountableDevice():
+    name: str
+    path: str
+    mountpoint: Optional[str]
+
+
+def resolve_mountable_devices(block_devices: List[BlockDevice]) -> List[MountableDevice]:
+    mountable_devices: List[MountableDevice] = []
+    for block_device in block_devices:
+        if block_device.tran is not None or block_device.tran is "usb":
+            continue
+
+        mountable_devices.append(MountableDevice(
+            name="whatever",
+            path=block_device.path,
+            mountpoint=block_device.mountpoint,
+        ))
+    return mountable_devices
+
+
 def get_block_devices() -> List[BlockDevice]:
     cmd: list[str] = [
         "lsblk", "--bytes",
-        "--output", "PATH,FSTYPE,LABEL,MOUNTPOINT,TYPE,SIZE",
+        "--output", "PATH,FSTYPE,LABEL,MOUNTPOINT,TRAN,TYPE,SIZE",
         "--json"
     ]
 
@@ -36,6 +58,7 @@ def get_block_devices() -> List[BlockDevice]:
             path=dev.get("path"),
             fstype=dev.get("fstype"),
             label=dev.get("label"),
+            tran=dev.get("tran"),
             mountpoint=dev.get("mountpoint"),
             type=dev.get("type"),
             size=int(dev["size"]) if dev.get("size") is not None else None,
@@ -52,4 +75,6 @@ def get_block_devices() -> List[BlockDevice]:
 
 if __name__ == "__main__":
     block_devices = get_block_devices()
-    pprint(block_devices, width=120)
+    pprint(block_devices)
+    mountable_devices = resolve_mountable_devices(block_devices)
+    pprint(mountable_devices, width=120)
