ad51394f1209102b2c041bf2aebc86ad63983d3e
Vaclav Kuzel <vkuzel@gmail.com>
2025-11-25 09:56:10 +0100

    Backup Firefox bookmarks.

--- home/.local/bin/backup.sh
+++ home/.local/bin/backup.sh
@@ -6,6 +6,18 @@ error() {
   exit 1
 }
 
+prepareFirefoxBackupInto() {
+  local tmpDir="$1"
+  local backupDir="$tmpDir/firefox-bookmarks"
+
+  mkdir "$backupDir" || error "Cannot create directory $backupDir"
+  find $HOME/.mozilla/firefox/*.default-release/bookmarkbackups \
+    -name "bookmarks*jsonlz4" \
+    -exec cp {} "$backupDir" \;
+
+  echo "$backupDir"
+}
+
 backupDevice="$1"
 if [[ -z "$backupDevice" ]]; then
   firstDevice=$(ls /media/ | grep "encrypted" | sort | head -1)
@@ -22,6 +34,10 @@ backupDir="$backupDevice/$hostname/$currentYear"
 read -p "Backup into $backupDir [y/N]" answer
 [[ "y" == "$answer" || "Y" == "$answer" ]] || exit
 
+tmpDir=$(mktemp --directory --suffix=backup)
+
+firefoxBackupDir=$(prepareFirefoxBackupInto "$tmpDir")
+
 rsync \
 	--archive \
 	--delete \
@@ -37,4 +53,7 @@ rsync \
 	"$HOME/Music" \
 	"$HOME/Videos" \
 	"$HOME/Pictures" \
+  "$firefoxBackupDir" \
 	"$backupDir"
+
+rm -r "$tmpDir"
