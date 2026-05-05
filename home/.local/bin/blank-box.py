e5d93117953758803314dbc430f1858c45740610
vkuzel <vkuzel@gmail.com>
2026-05-05 11:08:00 +0200

    Draw blank-box striped background - more pleasant to look at.

--- home/.local/bin/blank-box.py
+++ home/.local/bin/blank-box.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
+from tkinter import Canvas
+
+
+def render_stripes(canvas: Canvas):
+    width = canvas.winfo_width()
+    height = canvas.winfo_height()
+
+    for i in range(0, width + height, 20):
+        x1 = max(0, i - height)
+        y1 = min(i, height)
+        x2 = min(i, width)
+        y2 = max(0, i - width)
+        canvas.create_line(x1, y1, x2, y2, width=2, fill="#01302f")
 
 
 def main():
     root = tk.Tk()
     root.title("Blank Box")
-    root.configure(bg='#01302f')
+
+    canvas = tk.Canvas(root, bg="#00201e", highlightthickness=0)
+    canvas.pack(fill="both", expand=True)
+    canvas.bind("<Configure>", lambda e: render_stripes(canvas))
 
     root.bind('<Escape>', lambda e: root.quit())
     root.bind('q', lambda e: root.quit())
