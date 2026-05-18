4e904ff588f1e8bb894055b77c982b381219ead6
vkuzel <vkuzel@gmail.com>
2026-05-18 09:45:36 +0200

    Better start apps API.

--- home/.local/bin/start-me-apps.py
+++ home/.local/bin/start-me-apps.py
@@ -38,14 +38,22 @@ def wait_for_app_to_start(pattern: str):
         time.sleep(1)
 
 
-def start_app_detached(*cmd: str):
+def start_app_detached(cmd: list[str]):
     subprocess.Popen(
-        args=list(cmd),
+        args=cmd,
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
         stdin=subprocess.DEVNULL,
         start_new_session=True
     )
+
+
+def ensure_app_running(check_running_pattern: str, cmd: list[str]):
+    if is_app_running(check_running_pattern):
+        return
+
+    start_app_detached(*cmd)
+    wait_for_app_to_start(check_running_pattern)
 
 
 def switch_to_workspace(workspace: int):
@@ -58,43 +66,43 @@ def switch_to_workspace(workspace: int):
 def main():
     switch_to_workspace(10)
 
-    thunderbird_pattern = "thunderbird"
-    if not is_app_running(thunderbird_pattern):
-        start_app_detached("thunderbird")
-        wait_for_app_to_start(thunderbird_pattern)
+    ensure_app_running(
+        check_running_pattern="thunderbird",
+        cmd=["thunderbird"],
+    )
 
-    signal_pattern = "signal"
-    if not is_app_running(signal_pattern):
-        start_app_detached("signal-desktop")
-        wait_for_app_to_start(signal_pattern)
+    ensure_app_running(
+        check_running_pattern="signal",
+        cmd=["signal-desktop"],
+    )
 
     switch_to_workspace(2)
 
-    firefox_pattern = "firefox"
-    if not is_app_running(firefox_pattern):
-        start_app_detached("firefox")
-        wait_for_app_to_start(firefox_pattern)
+    ensure_app_running(
+        check_running_pattern="firefox",
+        cmd=["firefox"],
+    )
 
     switch_to_workspace(3)
 
-    idea_pattern = "jetbrains-idea"
-    if not is_app_running(idea_pattern):
-        start_app_detached("gtk-launch", "jetbrains-idea-ef52faa1-3035-4ceb-a7cb-0dfdcf75b2e1.desktop")
-        wait_for_app_to_start(idea_pattern)
+    ensure_app_running(
+        check_running_pattern="jetbrains-idea",
+        cmd=["gtk-launch", "jetbrains-idea-ef52faa1-3035-4ceb-a7cb-0dfdcf75b2e1.desktop"],
+    )
 
     switch_to_workspace(7)
 
-    neo_vim_pattern = "nvim-qt"
-    if not is_app_running(neo_vim_pattern):
-        home = os.path.expanduser("~")
-        tmp_file = os.path.join(home, "Documents/tmp.md")
-        start_app_detached("nvim-qt", "--", "-p", tmp_file)
-        wait_for_app_to_start(neo_vim_pattern)
+    home = os.path.expanduser("~")
+    tmp_file = os.path.join(home, "Documents/tmp.md")
+    ensure_app_running(
+        check_running_pattern="nvim-qt",
+        cmd=["nvim-qt", "--", "-p", tmp_file],
+    )
 
-    blank_box_pattern = "Blank Box"
-    if not is_app_running(blank_box_pattern):
-        start_app_detached("blank-box")
-        wait_for_app_to_start(blank_box_pattern)
+    ensure_app_running(
+        check_running_pattern="Blank Box",
+        cmd=["blank-box"],
+    )
 
     switch_to_workspace(2)
 
