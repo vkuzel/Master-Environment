1e0d222c02ac94f8bef9cab4eec25c273430743a
vkuzel <vkuzel@gmail.com>
2026-09-22 10:48:11 +0200

    Remove Copilot shell.

--- cleanup.sh
+++ cleanup.sh
@@ -76,4 +76,5 @@ remove_link "$HOME/.local/bin/mountui.sh"
 remove_link "$HOME/.local/bin/start-me-apps"
 remove_link "$HOME/.local/bin/view-images.sh"
 remove_link "$HOME/.local/bin/dockershell.sh"
+remove_link "$HOME/.local/bin/copilotshell.sh"
 remove_group docker
