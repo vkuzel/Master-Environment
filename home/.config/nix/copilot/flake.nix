33895977f0dd1982209353fb2dac846c679273e8
vkuzel <vkuzel@gmail.com>
2026-09-23 09:25:10 +0200

    Copilot CLI 1.0.89-0

--- home/.config/nix/copilot/flake.nix
+++ home/.config/nix/copilot/flake.nix
@@ -13,10 +13,30 @@
             "github-copilot-cli"
           ];
       };
+
+      github-copilot-cli = pkgs.github-copilot-cli.overrideAttrs (old: {
+        version = "1.0.89-0";
+        src = pkgs.fetchurl {
+          url = "https://github.com/github/copilot-cli/releases/download/v1.0.89-0/github-copilot-1.0.89-0-linux-x64.tgz";
+          hash = "sha256-h8ZCuXWk27rUlroehYBvS3qywi6P7B/ySnAJvJ0HE5I=";
+        };
+        autoPatchelfIgnoreMissingDeps = old.autoPatchelfIgnoreMissingDeps ++ [
+          "libwebkit2gtk-4.1.so.0"
+          "libgtk-3.so.0"
+          "libgdk-3.so.0"
+          "libcairo.so.2"
+          "libgdk_pixbuf-2.0.so.0"
+          "libsoup-3.0.so.0"
+          "libjavascriptcoregtk-4.1.so.0"
+          "libwayland-client.so.0"
+          "libdbus-1.so.3"
+          "libxdo.so.3"
+        ];
+      });
     in {
       devShells.${system}.default = pkgs.mkShell {
         packages = [
-          pkgs.github-copilot-cli
+          github-copilot-cli
         ];
       };
     };
